be1ff24f0001497d963374aefc3b0ee7a6505383
Harmanjot14 <jotkharman@gmail.com>
2025-07-01 18:35:37 +0000

    create function modified in reminders.php

--- app/controllers/reminders.php
+++ app/controllers/reminders.php
@@ -10,8 +10,26 @@ class Reminders extends Controller {
     }
 
     public function create(){
-      $this->view('reminders/create');
-      die;
+      if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $user_id = $_SESSION['user_id'] ?? null;
+        $subject = $_POST['subject'] ?? null;;
+        
+        if($user_id && $subject){
+          $reminder = $this->model('Reminder');
+          $reminder->create_reminder($user_id, $subject);
+          $_SESSION['success'] = "Reminder created successfully";
+          header('Location: /reminders');
+          exit;
+        }
+        else{
+          $_SESSION['error'] = "Missing subject";
+          header('Location: /reminders/create');
+          exit;
+        }
+      }
+      else{
+        $this->view('reminders/create');
+      }
     }
 
 }
