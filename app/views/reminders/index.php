a3f0d49b23ca8719cb245220d6df349747e877fe
Harmanjot14 <jotkharman@gmail.com>
2025-07-01 15:29:45 +0000

    Modified reminder page
    Added: Add reminder button,
    Added <table> tag which have, id, subject, timestamp , update, delete, and completed buttons

--- app/views/reminders/index.php
+++ app/views/reminders/index.php
@@ -5,9 +5,37 @@
       <div class="row">
         <div>
            <h1>Reminders</h1>
+           <button>Add Reminder</button>
+           <br><br>
         </div>
       </div>
-    </div>
+      <table>
+        <thead>
+          <tr>
+            <th> Reminder ID</th>
+            <th> Subject</th>
+            <th> Timestamp</th>
+            <th> Actions</th>
+          </tr>
+        </thead>  
+        <tbody>
+          <?php
+            foreach ($data['reminders'] as $reminder){
+                echo "<tr>";
+                echo "<td>" . $reminder['id'] . "</td>";
+                echo "<td>" . $reminder['subject'] . "</td>";
+                echo "<td>" . $reminder['created_at'] . "</td>";
+                echo "<td>
+                        <button>Update</button>
+                        <button>Delete</button>
+                        <button>Completed</button>
+                      </td>";
+            }
+          ?>
+        </tbody>
+      </table>
+      
+  </div>
 
   <?php require_once 'app/views/templates/footer.php' ?>
     
