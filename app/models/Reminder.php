3eb5dba77dde5f06034440e48a872a5797624043
Harmanjot14 <jotkharman@gmail.com>
2025-07-01 14:29:56 +0000

    Added functions update, create, delete and completed

--- app/models/Reminder.php
+++ app/models/Reminder.php
@@ -13,5 +13,30 @@ class Reminder{
         $row = $statement->fetchAll(PDO::FETCH_ASSOC);
         return $row;      
     }
+
+    /*function for update the reminder*/
+    public function update_reminder($reminder_id){
+        $db = db_connect();
+       
+    }
+    /*function for create the reminder*/
+    public function create_reminder($reminder_id){
+        $db = db_connect();
+
+    }
+    /*function for delete the reminder*/
+    public function delete_reminder($reminder_id){
+        $db = db_connect();
+    }
+
+    /*function for completed reminder*/
+    public function completed_reminder($reminder_id){
+        $db = db_connect();
+    }
+  
+  
   
 }
+
+
+?>
